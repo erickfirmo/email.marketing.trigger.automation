0488cff72b3fafc256be2eabc16f48bd3a650102
Érick Firmo <erickluques@gmail.com>
2020-02-26 08:32:14 -0300

    Update AdminsTableSeeder.php

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -11,34 +11,10 @@ class AdminsTableSeeder extends Seeder
     public function run()
     {
         $admin = new App\Admin;
-        $admin->name = 'Leonardo Almeida';
-        $admin->email = 'leonardo.almeida@metodista.br';
-        $admin->foto_de_perfil = 'https://avatars3.githubusercontent.com/u/42948574?s=460&v=4';
-        $admin->password = bcrypt('psw');
-        $admin->remember_token = str_random(10);
-        $admin->save();
-
-        $admin = new App\Admin;
-        $admin->name = 'Thiago Tamosauskas';
-        $admin->email = 'thiago.tamosauskas@metodista.br';
-        $admin->foto_de_perfil = 'https://avatars2.githubusercontent.com/u/12496600?s=460&v=4';
-        $admin->password = bcrypt('psw');
-        $admin->remember_token = str_random(10);
-        $admin->save();
-
-        $admin = new App\Admin;
-        $admin->name = 'Érick Firmo';
-        $admin->email = 'erick.serrano@metodista.br';
-        $admin->foto_de_perfil = 'https://avatars3.githubusercontent.com/u/34639603?s=460&v=4';
-        $admin->password = bcrypt('psw');
-        $admin->remember_token = str_random(10);
-        $admin->save();
-
-        $admin = new App\Admin;
-        $admin->name = 'Fernando Zancopé';
-        $admin->email = 'fernando.zancope@metodista.br';
-        $admin->foto_de_perfil = 'https://avatars0.githubusercontent.com/u/53548067?s=460&v=4';
-        $admin->password = bcrypt('psw');
+        $admin->name = 'Visitante';
+        $admin->email = 'visitante@domain.br';
+        $admin->foto_de_perfil = '#';
+        $admin->password = bcrypt('321');
         $admin->remember_token = str_random(10);
         $admin->save();
     }
